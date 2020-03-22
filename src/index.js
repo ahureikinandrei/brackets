82883e4c3d51c8d46a6e73e0fa26ed0168b3595a
ANDREI AHUREIKIN <ahureikinandrei@gmail.com>
2020-03-22 16:57:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) { 
+  for (let i = 0; i < bracketsConfig.length; ) {
+    let bracketsElem = bracketsConfig[i].join('')
+    if (str.includes(bracketsElem)) {
+      str = str.replace(bracketsElem, '');
+      i = 0;
+    }
+    else {i++}
+  }
+  if ( str.length == 0) return true;
+  else return false; 
 }
